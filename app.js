f286c4b389b044ce7cb3cce62c115e22c05ed9cb
Garvit-235 <garvits235@gmail.com>
2025-04-02 15:15:22 +0530

    middlewares implementation

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("shriharivansh");
+app.use("/admin", (req, res, next) => {
+  console.log("authorizing admin");
+  const token = "radha";
+  const isAdminAuthorized = token === "radha";
+  if (!isAdminAuthorized) {
+    res.status(401).send("not allowed");
+  } else next();
+});
+
+app.get("/admin/getData", (req, res, next) => {
+  res.send("all data send");
+});
+
+app.get("/admin/deleteUser", (req, res, next) => {
+  res.send("user removed");
 });
 
 app.listen(port, () => {
